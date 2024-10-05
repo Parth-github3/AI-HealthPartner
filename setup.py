ee6f3bbe0c1e1b99b2a476d019c942da1512fe9f
Parth Maheshwari <148962439+Parth-github3@users.noreply.github.com>
2024-10-05 14:46:55 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name="HC_app",
+    version="0.1",
+    install_requires=["pip==24.2", "langchain_groq", "langchain_groq", "streamlit", "langchain_core.runnables", "langchain_core.prompts", "langchain_core.output_parsers",],
+)       
+        
+        
+        
+        
+        
+        
